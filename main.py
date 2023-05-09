ae198c03e08eb125c75cfce8d8624e6210e660b6
Nguyễn Khánh Thuận <leeboykt@gmail.com>
2023-05-10 02:45:11 +0700

    change capture location

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 from detect import get_text
 zalo_location = f"{os.environ['USERPROFILE']}/AppData/Local/Programs/Zalo/Zalo.exe"
 os.system(zalo_location)
@@ -9,27 +10,32 @@ pyautogui.hotkey('win', 'up')
 time.sleep(1)
 pyautogui.hotkey('alt', '2')
 import os
-os.makedirs("images", exist_ok=True)
+
+folder = f"images"
+os.makedirs(folder, exist_ok=True)
 import re
 i = 0
 
 def auto_get(i):
-    pyautogui.moveTo(1900,1000)
+    pyautogui.moveTo(100,100)
     locations = list(pyautogui.locateAllOnScreen('button.png'))
     print(locations)
     locations.pop(-1)
     locations.pop(-2)
     for location in locations:
         locationxy = pyautogui.center(location)
-        if locationxy.y > 900:
+        if locationxy.y > 920:
             break
-        pyautogui.click(*locationxy)
-        x, y = pyautogui.locateCenterOnScreen('xemthongtin.png')
+        # pyautogui.click(*locationxy)
+        j,k = locationxy
+        region  = (420,k+40, 350, 80)
+        pyautogui.screenshot(f'{folder}/{i}.png', region=region)
+        # x, y = pyautogui.locateCenterOnScreen('xemthongtin.png')
         # pyautogui.move(-30, 40) 
-        pyautogui.click(x,y)
-        time.sleep(1)
-        pyautogui.screenshot(f'images/{i}.png',region=(785,207, 350, 630))
-        pyautogui.press('esc')
+        # pyautogui.click(x,y)
+        # time.sleep(1)
+        # pyautogui.screenshot(f'images/{i}.png',region=(785,207, 350, 630))
+        # pyautogui.press('esc')
         i+=1
     return i
 
@@ -41,5 +47,5 @@ count  = int(r[0])
 import math
 for n in range(math.ceil(count/10)):
     i = auto_get(i)
-    pyautogui.moveTo(946,119)
+    pyautogui.moveTo(800,119)
     pyautogui.scroll(-1000)
