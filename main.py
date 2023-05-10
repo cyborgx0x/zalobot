f895a975c4955a5e5df59e6001f74b07832ac7f6
Nguyễn Khánh Thuận <leeboykt@gmail.com>
2023-05-11 06:04:47 +0700

    fix detection

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import os
 import datetime
-from detect import get_text
+import requests
+import pyautogui
+import time
+
+
 zalo_location = f"{os.environ['USERPROFILE']}/AppData/Local/Programs/Zalo/Zalo.exe"
 os.system(zalo_location)
-import time
 time.sleep(3)
-import pyautogui
 pyautogui.hotkey('win', 'up')
 time.sleep(1)
 pyautogui.hotkey('alt', '2')
-import os
 
 folder = f"images"
 os.makedirs(folder, exist_ok=True)
 import re
 i = 0
 
-def auto_get(i):
+def auto_get(i, count, status):
     pyautogui.moveTo(100,100)
     locations = list(pyautogui.locateAllOnScreen('button.png'))
     print(locations)
@@ -24,11 +25,11 @@ def auto_get(i):
     locations.pop(-2)
     for location in locations:
         locationxy = pyautogui.center(location)
-        if locationxy.y > 920:
-            break
+        # if locationxy.y > 920:
+        #     break
         # pyautogui.click(*locationxy)
         j,k = locationxy
-        region  = (420,k+40, 350, 80)
+        region  = (420,k-30, 450, 80)
         pyautogui.screenshot(f'{folder}/{i}.png', region=region)
         # x, y = pyautogui.locateCenterOnScreen('xemthongtin.png')
         # pyautogui.move(-30, 40) 
@@ -37,15 +38,22 @@ def auto_get(i):
         # pyautogui.screenshot(f'images/{i}.png',region=(785,207, 350, 630))
         # pyautogui.press('esc')
         i+=1
+        if i == count:
+            status = False
+            break
     return i
-
-banbe = pyautogui.screenshot(region=(416,103, 160, 60))
-banbe_count = get_text(banbe)
-print(banbe_count)
-r = re.findall(r'\d+', banbe_count)
+time.sleep(2)
+banbe = pyautogui.screenshot("count.png", region=(418,104, 160, 60))
+f = {'file': open("count.png","rb")}
+res = requests.post("https://detection.diopthe20.com/detect/", files=f)
+print(res.text)
+r = re.findall(r'\d+', res.text)
 count  = int(r[0])
+banbe = pyautogui.screenshot("count.png",region=(418,104, 160, 60))
+
 import math
-for n in range(math.ceil(count/10)):
-    i = auto_get(i)
+status = True 
+while status:
+    i = auto_get(i, count, status)
     pyautogui.moveTo(800,119)
-    pyautogui.scroll(-1000)
+    pyautogui.scroll(-700)
