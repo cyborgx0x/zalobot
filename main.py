df51cde9f548d4e3e3753c3afa860a80f49cbff6
Nguyễn Khánh Thuận <leeboykt@gmail.com>
2023-05-12 06:43:52 +0700

    change logic

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import os
-import datetime
 import requests
 import pyautogui
 import time
-
+import math
+from PIL import Image
+import cv2 as cv
+import numpy as np
+from matplotlib import pyplot as plt
+from pyautogui import locateAll
+import re
+from datetime import datetime
 
 zalo_location = f"{os.environ['USERPROFILE']}/AppData/Local/Programs/Zalo/Zalo.exe"
 os.system(zalo_location)
@@ -12,11 +18,8 @@ pyautogui.hotkey('win', 'up')
 time.sleep(1)
 pyautogui.hotkey('alt', '2')
 
-folder = f"images"
+folder = f"images"+str(datetime.now().timestamp())
 os.makedirs(folder, exist_ok=True)
-import re
-i = 0
-  
 time.sleep(2)
 banbe = pyautogui.screenshot("count.png", region=(418,104, 160, 60))
 f = {'file': open("count.png","rb")}
@@ -24,38 +27,35 @@ res = requests.post("https://detection.diopthe20.com/detect/", files=f)
 print(res.text)
 r = re.findall(r'\d+', res.text)
 count  = int(r[0])
-banbe = pyautogui.screenshot("count.png",region=(418,104, 160, 60))
 
-
-while i != count:
-    print(type(i))
-    print(type(count))
-    print(i==count)
-    pyautogui.moveTo(100,100)
-    locations = list(pyautogui.locateAllOnScreen('button.png'))
-    print(locations)
-    locations.pop(-1)
-    locations.pop(-2)
-    
-    for location in locations:
-        locationxy = pyautogui.center(location)
-        
-        # if locationxy.y > 920:
-        #     break
-        # pyautogui.click(*locationxy)
-        j,k = locationxy
-        region  = (420,k-30, 450, 80)
-        pyautogui.screenshot(f'{folder}/{i}.png', region=region)
-        # x, y = pyautogui.locateCenterOnScreen('xemthongtin.png')
-        # pyautogui.move(-30, 40) 
-        # pyautogui.click(x,y)
-        # time.sleep(1)
-        # pyautogui.screenshot(f'images/{i}.png',region=(785,207, 350, 630))
-        # pyautogui.press('esc')
-        i+=1
-        if count == i:
-            i = count
-            break
-    
+queue = []
+for i in range(math.ceil(count/10)):
     pyautogui.moveTo(800,119)
-    pyautogui.scroll(-700)
+    pyautogui.scroll(-1000)
+    queue.append(pyautogui.screenshot())
+i = 0
+crop_image_queue = []
+for image in queue:
+
+    # image = Image.open("bgf.png").convert('RGB') 
+    open_cv_image = np.array(image) 
+    open_cv_image = open_cv_image[:, :, ::-1].copy() 
+    button = cv.imread('button.png')
+    res = locateAll(button, open_cv_image)
+
+    for box in res:
+        if i == count:
+            break
+        y = box.top-30
+        x = 420
+        h = 80
+        w = 450
+        crop_img = open_cv_image[y:y+h, x:x+w]
+        crop_image_queue.append(crop_img)
+        i+=1
+
+print(len(crop_image_queue))
+
+for index, img in enumerate(crop_image_queue):
+    cv.imwrite(f'{folder}/{index}.png',img)
+
